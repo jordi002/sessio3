2f91e2e979e3eea81da17122e30068ce636e22cb
Jordi Godia <alcarras.jordi.godia@gmail.com>
2017-03-27 11:13:59 +0200

    Ok

--- sobres/isobres/views.py
+++ sobres/isobres/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def homepage(request):
+    page = """
+    <html>
+    <body>
+    Bribes management application
+    (c) Luis Barcenas, 2017
+    </body>
+    </html>
+    """
+    return HttpResponse(page)
--- sobres/sobres/urls.py
+++ sobres/sobres/urls.py
@@ -15,6 +15,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from isobres.views import *
+
 urlpatterns = [
+    url(r'^dashboard/(\w+)/$', dashboard),
+
+    url(r'^$', homepage),
+
     url(r'^admin/', include(admin.site.urls)),
 ]
